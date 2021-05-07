1c59b07211853b190914cd89d7f1f481b1aee0c2
DominiquevdLinden <vdlindendominique33@gmail.com>
2021-05-07 16:32:27 +0200

    pokemon components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./components/Title";
+import Pokemon from "./components/Pokemon";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <main>
+        <Title content="My baby is so cute" />
+        <Pokemon
+          name="Charizard"
+          weight={90}
+          awesome={true}
+          terrifying={false}
+          abilities={["Blaze", "Solar power", "Tough claws", "Drought"]}
+        />
+        <Pokemon
+          name="Bulbasaur"
+          weight={6.9}
+          awesome={true}
+          terrifying={false}
+          abilities={["Overgrow", "Chlorophyll"]}
+        />
+        <Pokemon
+          name="Mewtwo"
+          weight={122}
+          awesome={true}
+          terrifying={true}
+          abilities={["Intimidate", "Unnerve"]}
+        />
+      </main>
     </div>
   );
 }
